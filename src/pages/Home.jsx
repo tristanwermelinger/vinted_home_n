4107fb2dc98b77d5e889be0e3114e28efe354349
tristan <tristan.wermelinger@gmail.com>
2023-05-18 15:21:34 +0200

    success affiché

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,6 +16,7 @@ const Home = () => {
           "https://lereacteur-vinted-api.herokuapp.com/offers/"
         );
         setData(response.data);
+        console.log(response.data);
         setIsLoading(false);
       } catch (error) {
         console.log(error.message);
@@ -28,44 +29,7 @@ const Home = () => {
     <p>Loading ...</p>
   ) : (
     <div>
-      {/* <p>Nombre d'offres : {data.count}</p> */}
-      <img className="bannersize" src={banner} alt="" />
-      <br />
-      <br />
-      <div className="fleximages">
-        {data.offers.map((offer, index) => {
-          console.log(offer);
-
-          return (
-            <div key={offer.id}>
-              {/* <div key={offer.id}>{offer.product_pictures[0].secure_url}</div> */}
-              <div className="margindiv" key={offer.id}>
-                {offer.owner.account.username}
-              </div>
-              <div>
-                <Link to={`/offer/${offer._id}`}>
-                  <img
-                    // onClick={() => {
-                    //   offer._id;
-                    // }}
-                    className="size"
-                    key={offer.id}
-                    src={offer.product_image.secure_url}
-                    alt="images"
-                  />
-                </Link>
-              </div>
-              <div key={offer.id}>{offer.product_price} €</div>
-              <div key={offer.id}>
-                {offer.product_details[1].TAILLE
-                  ? offer.product_details[1].TAILLE
-                  : ""}
-              </div>
-              <div key={offer.id}>{offer.product_details[0].MARQUE}</div>
-            </div>
-          );
-        })}
-      </div>
+      <p>success</p>
     </div>
   );
 };
