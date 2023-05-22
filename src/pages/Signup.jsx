9606e1e3187f6a9db1b4a27edf6de5632c60b77a
tristan <tristan.wermelinger@gmail.com>
2023-05-22 09:32:37 +0200

    commit monday

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -6,27 +6,60 @@ const Signup = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  // const handleSubmit = async (event) => {
-  //   try {
-  //     event.preventDefault();
-  //     const response = await axios.post(
-  //       "https://lereacteur-vinted-api.herokuapp.com/user/signup"
-  //     );
+  const handleUserNameChange = (event) => {
+    const value = event.target.value;
+    setUsername(value);
+  };
 
-  //   } catch (error) {
-  //     console.log(error.message);
-  //   }
-  // };
+  const handelEmailChange = (event) => {
+    const value = event.target.value;
+    setEmail(value);
+  };
+
+  const handlePasswordChange = (event) => {
+    const value = event.target.value;
+    setPassword(value);
+  };
+
+  const handleSubmit = async (event) => {
+    try {
+      event.preventDefault();
+      const response = await axios.post(
+        "https://lereacteur-vinted-api.herokuapp.com/user/signup"
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   return (
     <div>
       <h2>S'inscrire</h2>
-      <form id="contactForm">
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">User Name :</label>
         <input
-          type="text"
-          id="username"
-          value="username"
           placeholder="Nom d'utilisateur"
+          type="text"
+          name="username"
+          value={username}
+          onChange={handleUserNameChange}
+        />
+
+        <label htmlFor="email">Email :</label>
+        <input
+          placeholder="Votre email"
+          type="text"
+          name="email"
+          value={email}
+          onChange={handelEmailChange}
+        />
+        <label htmlFor="password">Password :</label>
+        <input
+          placeholder="Votre password"
+          type="password"
+          name="password"
+          value={password}
+          onChange={handlePasswordChange}
         />
       </form>
     </div>
